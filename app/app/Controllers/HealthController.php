c1c5251816748a7cedbcd883f49907a99ec27212
Bartosz Gromowski <gromula@bartoszs-mbp.home>
2025-01-30 20:59:45 +0100

    add health controller for checking connection with redis

---
+++ app/app/Controllers/HealthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use Predis\Client;
+
+class HealthController extends Controller
+{
+    public function index()
+    {
+        try {
+            $redis = new Client([
+                'host' => 'redis',
+                'port' => 6379
+            ]);
+
+            $redis->set('health_check', 'OK');
+            if ($redis->get('health_check') === 'OK') {
+                $status['redis'] = 'Connected';
+            }
+        } catch (\Exception $e) {
+            $status['redis'] = 'Error: ' . $e->getMessage();
+        }
+
+        return $this->response->setJSON($status);
+    }
+}
